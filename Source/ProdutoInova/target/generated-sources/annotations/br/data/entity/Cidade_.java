df7d395759cbb7d099a5318ade5c3ee45a61bb37
João Goulart <jv.goulart.almeida@hotmail.com>
2018-12-03 23:19:07 -0200

    jar cli4

---
+++ Source/ProdutoInova/target/generated-sources/annotations/br/data/entity/Cidade_.java
@@ -0,0 +1,16 @@
+package br.data.entity;
+
+import br.data.entity.Estado;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-03T23:16:32")
+@StaticMetamodel(Cidade.class)
+public class Cidade_ { 
+
+    public static volatile SingularAttribute<Cidade, Integer> codigo;
+    public static volatile SingularAttribute<Cidade, Estado> estado;
+    public static volatile SingularAttribute<Cidade, String> nome;
+
+}
---
+++ Source/ProdutoInova/target/generated-sources/annotations/br/data/entity/Estado_.java
@@ -0,0 +1,15 @@
+package br.data.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-03T23:16:32")
+@StaticMetamodel(Estado.class)
+public class Estado_ { 
+
+    public static volatile SingularAttribute<Estado, String> uf;
+    public static volatile SingularAttribute<Estado, Integer> codigo;
+    public static volatile SingularAttribute<Estado, String> nome;
+
+}
